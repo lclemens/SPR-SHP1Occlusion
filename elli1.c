884b5acdbcdde6a1add8892a120e1d274308ed73
Lara Clemens <lclemens@uci.edu>
2017-01-10 13:42:22 -0800

    fix upper box wall constraint error

--- elli1.c
+++ elli1.c
@@ -110,7 +110,7 @@ void elliMetropolis()
             if (membraneTF)
             {
                 // if the domain sphere would exceed the box constraints in any direction
-                if (   rPropose[iPropose][2] < radius[iPropose] || rPropose[iPropose][2] > boxSize
+                if (   rPropose[iPropose][2] < radius[iPropose] || rPropose[iPropose][2] > (boxSize-radius[iPropose])
                     || rPropose[iPropose][0] < -(boxSize/2-radius[iPropose]) || rPropose[iPropose][0] > +(boxSize/2-radius[iPropose])
                     || rPropose[iPropose][1] < -(boxSize/2-radius[iPropose]) || rPropose[iPropose][1] > +(boxSize/2-radius[iPropose]))
                 {
